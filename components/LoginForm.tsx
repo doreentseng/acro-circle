f469d0a85112308b09b239efc7b2622a413fbe6a
doreentseng <doreentseng52@gmail.com>
2026-06-03 17:50:54 +0800

    feat(login-form): complete UI/UX of login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -3,16 +3,20 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/lib/supabase';
+import PageHeader from './ui/PageHeader';
+import { inputClass } from '@/lib/styles/input';
 
 export default function LoginForm() {
   const [username, setUsername] = useState('');
   const [sesame, setSesame] = useState('');
   const [error, setError] = useState('');
+  const [isEntering, setIsEntering] = useState(false);
 
   const router = useRouter();
 
-  async function handleLogin() {
+  const handleLogin = async () => {
     setError('');
+    setIsEntering(true);
 
     // get email by username
     const { data, error } = await supabase
@@ -25,6 +29,7 @@ export default function LoginForm() {
 
     if (error || !data) {
       setError('查無此使用者');
+      setIsEntering(false);
       return;
     }
     console.log('Found data:', data);
@@ -39,6 +44,7 @@ export default function LoginForm() {
 
     if (authError) {
       setError('密碼錯誤');
+      setIsEntering(false);
       return;
     }
 
@@ -47,44 +53,49 @@ export default function LoginForm() {
         userId: String(data.id),
       },
     });
-
+    setIsEntering(false);
     router.push('/dashboard');
-  }
+  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleLogin();
-      }}
-      className="w-80 rounded-xl bg-white p-6 shadow"
-    >
-      <h1 className="mb-4 text-lg font-semibold">Acro Circle</h1>
+    <div className="min-h-screen bg-zinc-100 flex justify-center items-start pt-30 px-6">
+      <div className="w-full max-w-6xl">
+        <PageHeader />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+          className="w-80 rounded-md bg-white p-6 shadow space-y-3"
+        >
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value.toLowerCase())}
+            placeholder="輸入帳號"
+            className={inputClass}
+          />
+          <input
+            type="password"
+            value={sesame}
+            onChange={(e) => setSesame(e.target.value)}
+            placeholder="輸入密碼"
+            className={inputClass}
+          />
 
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="輸入帳號"
-        className="w-full rounded border px-3 py-2 mb-2"
-      />
+          {error && <p className=" text-sm text-red-500">{error}</p>}
 
-      <input
-        type="password"
-        value={sesame}
-        onChange={(e) => setSesame(e.target.value)}
-        placeholder="輸入密碼"
-        className="w-full rounded border px-3 py-2"
-      />
-
-      {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
-
-      <button
-        onClick={handleLogin}
-        className="mt-4 w-full rounded bg-zinc-900 py-2 text-white"
-      >
-        進入
-      </button>
-    </form>
+          <button
+            type="submit"
+            disabled={isEntering}
+            className="w-full px-4 py-2 rounded-md bg-pink-500 text-white transition cursor-pointer
+              hover:bg-pink-400
+              disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-pink-400"
+          >
+            {isEntering ? '驗證中...' : '進入'}
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
